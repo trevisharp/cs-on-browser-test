d58ba6256f82f509bd1e7c53356e7e6b3a3b8ebc
trevisharp <leonardotrevisansilio@gmail.com>
2024-12-06 10:02:33 -0300

    add script to extract webassembly

--- test.ps1
+++ test.ps1
@@ -1,1 +1,3 @@
 dotnet publish -c Release
+$url = ".\Lib\bin\Release\net8.0-browser\publish\wwwroot\_framework\"
+ls $url | ? { $_.Name.EndsWith(".wasm") } | % { cp ($url + $_.Name) (".\Web\" + $_.Name)  }
