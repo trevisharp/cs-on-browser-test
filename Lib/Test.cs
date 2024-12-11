d091fe5b101f323a8849ecbe7928288e4f79067b
trevisharp <leonardotrevisansilio@gmail.com>
2024-12-11 12:05:17 -0300

    start tests with assembly

---
+++ Lib/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.JSInterop;
+
+namespace Lib;
+
+// doc: https://yangzhongke8.medium.com/without-blazor-webassembly-develop-a-web-site-that-compiles-and-runs-c-code-on-browser-c381873f6d03
+public static partial class Test
+{
+    [JSInvokable]
+    public static float Sum(float a, float b)
+        => a + b;
+    
+    [JSInvokable]
+    public static float[] Sort(params float[] array)
+        => [ ..array.OrderBy(x => x) ];
+}
--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,3 @@
 dotnet workload install wasm-tools
+dotnet add package Microsoft.AspNetCore.Components.WebAssembly
+dotnet add package Microsoft.AspNetCore.Components.WebAssembly.DevServer
